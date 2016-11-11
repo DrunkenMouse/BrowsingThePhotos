9c8fbff38a1fe6f3b54fbbbfe2385c49487bdd28
残月 <632342527@qq.com>
2016-11-11 15:44:47 +0800

    Initial Commit

---
+++ 浏览商品图片/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  浏览商品图片
+//
+//  Created by 王奥东 on 16/11/11.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
